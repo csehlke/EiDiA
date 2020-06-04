75aa698d6573c59c5b85b0ac187ed9c15e45a9a0
Tom Bader <tom@tombader.de>
2020-06-04 23:51:32 +0200

    Erster Header-Entwurf

--- EiDiA_frontend/src/components/Header.js
+++ EiDiA_frontend/src/components/Header.js
@@ -1,22 +1,92 @@
 "use strict";
 
 import React from 'react';
-import { withRouter } from 'react-router-dom'
+import { makeStyles } from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import IconButton from "@material-ui/core/IconButton";
+import AccountCircle from "@material-ui/icons/AccountCircle";
+import MenuItem from "@material-ui/core/MenuItem";
+import Menu from "@material-ui/core/Menu";
 
+import logo from "../assets/logo.png"
 
-class Header extends React.Component {
+// SIEHE: https://material-ui.com/components/app-bar/
 
-    constructor(props) {
-        super(props);
+const useStyles = makeStyles(theme => ({
+    root: {
+        flexGrow: 1
+    },
+    menuButton: {
+        marginRight: theme.spacing(2)
+    },
+    title: {
+        flexGrow: 1
+    },
+    logo: {
+        width: "9%"
     }
+}));
 
-    render() {
-        return (
-            <div>
-                Here is the header
-            </div>
-        );
-    }
+export default function MenuAppBar() {
+    const classes = useStyles();
+    const [anchorEl, setAnchorEl] = React.useState(null);
+    const open = Boolean(anchorEl);
+
+
+
+    const handleMenu = event => {
+        setAnchorEl(event.currentTarget);
+    };
+
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
+
+    return (
+        <div className={classes.root}>
+            <AppBar position="static">
+                <Toolbar>
+                    <img
+                        className={classes.logo}
+                        src={logo}
+                        alt="Logo"
+                    />
+                    <Typography variant="h6" align="center" className={classes.title}>
+                        PUT YOUR TITLE HERE
+                    </Typography>
+                    <div>
+                        <IconButton
+                            aria-label="account of current user"
+                            aria-controls="menu-appbar"
+                            aria-haspopup="true"
+                            onClick={handleMenu}
+                            color="inherit"
+                        >
+                            <AccountCircle />
+                        </IconButton>
+                        <Menu
+                            id="menu-appbar"
+                            anchorEl={anchorEl}
+                            anchorOrigin={{
+                                vertical: "top",
+                                horizontal: "right"
+                            }}
+                            keepMounted
+                            transformOrigin={{
+                                vertical: "top",
+                                horizontal: "right"
+                            }}
+                            open={open}
+                            onClose={handleClose}
+                        >
+                            <MenuItem onClick={handleClose}>Profile</MenuItem>
+                            <MenuItem onClick={handleClose}>My account</MenuItem>
+                        </Menu>
+                    </div>
+                </Toolbar>
+            </AppBar>
+        </div>
+    );
 }
-
-export default withRouter(Header);
