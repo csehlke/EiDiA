1274f46f9122a60240fb30cc0ede6286cfbdbe93
Carsten Sehlke <carsten.sehlke@tum.de>
2020-06-08 22:12:24 +0200

    Sidebar

---
+++ EiDiA_frontend/src/assets/NewFile.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const NewFile = () =>
+    <svg enableBackground="new 0 0 512 512"
+         height="1em"
+         width="1em"
+         fontSize="1.5rem"
+         fill='currentColor'
+         display='inline-block'
+         color='rgba(0, 0, 0, 0.54)'
+         viewBox="0 0 512 512"
+         xmlns="http://www.w3.org/2000/svg">
+        <path d="m503.01 220.771-104.383-104.384v104.384z"/>
+        <path d="m368.601 107.592h-260.91v404.31h404.309v-261.105h-143.399z"/>
+        <path d="m107.691.098h30.026v70.185h-30.026z"/>
+        <path d="m0 106.27h70.185v30.026h-70.185z"/>
+    </svg>;
+
+export default NewFile;
---
+++ EiDiA_frontend/src/components/Link.js
@@ -0,0 +1,7 @@
+import {Link as NormalLink} from 'react-router-dom'
+import Styled from 'styled-components'
+
+export const Link = Styled(NormalLink)`
+    text-decoration: none;
+    color: inherit;
+`;
