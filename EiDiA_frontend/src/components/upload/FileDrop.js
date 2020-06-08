85bc267ee988ab54eef71339f1823840a9d0401e
Tom Bader <tom@tombader.de>
2020-06-08 12:03:45 +0200

    Added Button

--- EiDiA_frontend/src/components/upload/FileDrop.js
+++ EiDiA_frontend/src/components/upload/FileDrop.js
@@ -2,6 +2,7 @@
 
 import React from 'react';
 import styled from "styled-components";
+import {Button} from "@material-ui/core";
 
 const Container = styled.div`
     flex-grow: 1;
@@ -16,13 +17,22 @@ class FileDrop extends React.Component {
         }
     }
 
+    assignRecord() {
+        console.log("Open FilePicker here");
+    }
+
     render() {
         return (
             <Container>
-
-                <div>Hier DragDrop</div>
-
-
+                <div>
+                    <Button
+                        variant="contained"
+                        color="primary"
+                        onClick={this.assignRecord}
+                        style={{width: '20%', margin: '0.5em'}}>
+                        Assign to Record
+                    </Button>
+                </div>
             </Container>
         )
     }
