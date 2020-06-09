bdb24343e3219dcb4633992ae09c3657bb26d893
Tom Bader <tom@tombader.de>
2020-06-09 11:12:34 +0200

    Added SmartDropDownBox (from branch features/csehlke/search_form_basic)

---
+++ EiDiA_frontend/src/components/SmartDropDownBox.js
@@ -0,0 +1,63 @@
+import React from "react";
+import TextField from "@material-ui/core/TextField";
+import Autocomplete from "@material-ui/lab/Autocomplete";
+import PropTypes from "prop-types";
+
+class SmartDropDownBox extends React.Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            inputValue: ''
+        }
+
+        this.handleOnChange = this.handleOnChange.bind(this);
+        this.reset = this.reset.bind(this);
+    }
+
+    reset() {
+        this.setState({
+            inputValue: ''
+        });
+    }
+
+    handleOnChange(event, value) {
+        this.setState({
+            inputValue: value.name
+        });
+        this.props.onChange(event, value);
+    }
+
+    render() {
+        return (
+            <Autocomplete
+                id="combo-box"
+                handleHomeEndKeys
+                selectOnFocus
+                autoComplete
+                autoHighlight
+                blurOnSelect
+                fullWidth
+                options={this.props.options}
+                inputValue={this.state.inputValue}
+                onChange={this.handleOnChange}
+                getOptionLabel={(option) => option.name}
+                style={{margin: '0.5em'}}
+                renderInput={(params) => (
+                    <TextField {...params}
+                               label={this.props.label}
+                               variant="outlined" placeholder="Type to filter"/>
+                )}
+            />
+        );
+    }
+}
+
+export default SmartDropDownBox;
+
+SmartDropDownBox.propTypes = {
+    label: PropTypes.string.isRequired,
+    onChange: PropTypes.func.isRequired,
+    options: PropTypes.array.isRequired
+}
