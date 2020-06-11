627e854fc8e5b26f2c661689e21b4d95b90d57cb
Philipp Bock <philipp.bock@hotmail.de>
2020-06-11 10:20:37 +0200

    Added FileExplorer.js, missing in previous commit

---
+++ EiDiA_frontend/src/components/fileCabinet/FileExplorer.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {Element} from '../filetable/Element'
+
+export class FileExplorer extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return (
+            <div>
+                <Element></Element>
+            </div>
+        );
+    }
+}
+
