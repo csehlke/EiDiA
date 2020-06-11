217aae765d63682e962aa992240bb950f5f1bf7e
Philipp Bock <philipp.bock@hotmail.de>
2020-06-11 12:38:07 +0200

    Adjusted FileExplorer & Element styles Added ElementActions.js

--- EiDiA_frontend/src/components/fileCabinet/FileExplorer.js
+++ EiDiA_frontend/src/components/fileCabinet/FileExplorer.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import {Element} from '../filetable/Element'
-
+import styled from "styled-components";
+const Divider = styled.hr`
+    margin: 0 5vw  0 5vw;
+    
+`;
 export class FileExplorer extends React.Component {
     constructor(props) {
         super(props);
@@ -8,7 +12,9 @@ export class FileExplorer extends React.Component {
     render() {
         return (
             <div>
-                <Element></Element>
+                <Element type ={'HEADING'} name = {'Name'} dateCreation = {'Date'} dateModification = {'Last Modified'} comment = {'Comment'} actions={['HEADING']}/>
+                <Divider/>
+                <Element type = {'WORD'} name= {'Sample Word Document'} dateCreation = {'1.1.1970'} dateModification = {'1.1.2020'} comment = {'This is a long long Lorem Ipsum Comment talking blablabla'} actions={['DOWNLOAD','EDIT','DELETE']}/>
             </div>
         );
     }
