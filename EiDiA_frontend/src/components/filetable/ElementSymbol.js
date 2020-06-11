4fe71c56ac488cd4c40889fa55c2ec5aca6e7f14
Philipp Bock <philipp.bock@hotmail.de>
2020-06-11 10:22:06 +0200

    Optimize Imports

--- EiDiA_frontend/src/components/filetable/ElementSymbol.js
+++ EiDiA_frontend/src/components/filetable/ElementSymbol.js
@@ -1,14 +1,15 @@
 import React from 'react';
-import styled from "styled-components";
-import {FaFileWord} from 'react-icons/fa'
-import {FaFilePdf} from 'react-icons/fa'
+import {FaFilePdf, FaFileWord} from 'react-icons/fa'
 import {AiFillFileUnknown} from 'react-icons/ai'
 
 export class ElementSymbol extends React.Component {
     constructor(props) {
         super(props);
     }
-
+    /*
+     *TODO:
+     * - Add more symbols like folder, excel etc.
+     */
     symbolSelection(type){
         switch(type){
             case 'pdf':
