c9024e32dff77f1a692c2b9f48990f93504f64ff
Tom Bader <tom@tombader.de>
2020-06-17 16:26:08 +0200

    Added dynamic rendering of Textfields

--- EiDiA_frontend/src/components/upload/AttributeContainer.js
+++ EiDiA_frontend/src/components/upload/AttributeContainer.js
@@ -4,6 +4,11 @@ import React from 'react';
 import styled from "styled-components";
 
 import {createWorker} from 'tesseract.js';
+import TextField from "@material-ui/core/TextField";
+import IconButton from "@material-ui/core/IconButton";
+import InputAdornment from "@material-ui/core/InputAdornment";
+import CropIcon from "@material-ui/icons/Crop";
+import FormControl from "@material-ui/core/FormControl";
 
 
 const Container = styled.div
@@ -21,9 +26,14 @@ class AttributeContainer extends React.Component {
         super(props);
 
         this.state = {
-            wrk: this.loadWorker()
+            attributes: [
+                {name: 'Attr1', id: '1'},
+                {name: 'Attr2', id: '2'},
+                {name: 'Attr3', id: '3'}
+            ],
+            wrk: this.loadWorker(),
+            textValue: "",
         }
-
 
     }
 
@@ -45,37 +55,62 @@ class AttributeContainer extends React.Component {
 
 
     componentDidMount() {
-
     }
 
-    sendFulltextToBackend(){ //TODO Send to Backend (OCR IS VERY SLOW) --> Do it on backend?
-         const worker = createWorker({
-             logger: m => console.log(m)
-         });
-
-         (async () => {
-             await worker.load();
-             await worker.loadLanguage('eng');
-             await worker.initialize('eng');
-             const { data: { text } } = await worker.recognize(this.props.picture[0]);
-             console.log(text);
-             await worker.terminate();
-         })();
+    renderTextFields(attrName, attrID) {
+        return <TextField
+            label={attrName}
+            variant="outlined"
+            value={this.state.textValue}
+            //onChange={}
+            //inputRef={}
+            //onBlur={}
+            InputProps={{
+                endAdornment: <InputAdornment>
+                    <IconButton /*onClick={}*/>
+                        <CropIcon/>
+                    </IconButton>
+                </InputAdornment>
+            }}
+        />
     }
 
+    sendFulltextToBackend() { //TODO Send to Backend (OCR IS VERY SLOW) --> Do it on backend?
+        const worker = createWorker({
+            logger: m => console.log(m)
+        });
 
-    componentDidUpdate(prevProps) { //When new crop gets added
         (async () => {
-            const {data: {text}} = await this.state.wrk.recognize(this.props.crop); //Do OCR on crop
+            await worker.load();
+            await worker.loadLanguage('eng');
+            await worker.initialize('eng');
+            const {data: {text}} = await worker.recognize(this.props.picture[0]);
             console.log(text);
+            await worker.terminate();
         })();
     }
 
 
+    componentDidUpdate(prevProps) {
+        if (prevProps.crop !== this.props.crop) { //When new crop gets added
+            (async () => {
+                const {data: {text}} = await this.state.wrk.recognize(this.props.crop); //Do OCR on crop
+                console.log(text);
+                this.setState({
+                    textValue: text
+                });
+            })();
+
+        }
+    }
+
+
     render() {
-        return (
+        return ( // Render TextFields dynamically from state
             <Container>
-                <div>Attributecontainer</div>
+                <FormControl>
+                    {this.state.attributes.map((item) => <div key={item.id}>{this.renderTextFields(item.name, item.id)}</div>)}
+                </FormControl>
             </Container>
         )
     }
