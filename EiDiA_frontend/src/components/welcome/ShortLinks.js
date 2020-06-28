fe668bab276a253e0a932ab1b7985684a37a48c1
Carsten Sehlke <carsten.sehlke@tum.de>
2020-06-28 14:09:23 +0200

    UserInfo

---
+++ EiDiA_frontend/src/components/welcome/ShortLinks.js
@@ -0,0 +1,18 @@
+"use strict";
+
+import React from 'react';
+
+export default class ShortLinks extends React.Component {
+
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div>
+                ShortLinks works
+            </div>
+        );
+    }
+}
