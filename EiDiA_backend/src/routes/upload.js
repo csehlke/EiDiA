ca8192d365748b35922654c0cc3912c461f3d561
Tom Bader <tom@tombader.de>
2020-06-28 14:45:35 +0200

    Setup API + Frontend call to API

--- EiDiA_backend/src/routes/upload.js
+++ EiDiA_backend/src/routes/upload.js
@@ -6,6 +6,8 @@ const router = express.Router();
 const middleWares = require('../middlewares');
 const UploadController = require('../controllers/upload');
 
+router.get('/attributes/get/:selectedDocumentTypeId', middleWares.checkAuthentication, UploadController.listAttributes);
+
 router.post('/document', middleWares.checkAuthentication, UploadController.uploadDocument);
 router.post('/data', middleWares.checkAuthentication, UploadController.addAttributes);
 
--- EiDiA_frontend/src/services/UploadService.js
+++ EiDiA_frontend/src/services/UploadService.js
@@ -1,10 +1,21 @@
 "use strict";
 
+import HttpService from './HttpService';
+import {baseURL} from "../../../constants";
+
 export default class UploadService {
     constructor() {
     }
 
-//TODO Add Upload API-calls here
+    static listAttributes(documentTypeID) {
+        return new Promise((resolve, reject) => {
+            HttpService.get(baseURL + '/upload/attributes/get/' + documentTypeID, function (data) {
+                resolve(data);
+            }, function (textStatus) {
+                reject(textStatus);
+            });
+        });
+    }
 
 
 }
