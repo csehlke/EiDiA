65c13241450b9a32e22eca9949bfa32485fb7f1d
Philipp Bock <philipp.bock@hotmail.de>
2020-06-30 18:26:09 +0200

    Fixed Styles for "full" height in Dashboard

--- EiDiA_frontend/src/components/StyleElements.js
+++ EiDiA_frontend/src/components/StyleElements.js
@@ -1,7 +1,7 @@
 import styled from "styled-components";
 
 export const WrapperRecordView = styled.div`
-    height: 50vh;
+    height: 70vh;
     margin: 0 5% 0 5%;
     
     
@@ -9,7 +9,7 @@ export const WrapperRecordView = styled.div`
 
 `;
 export const Grid3Cols2Rows = styled.div`
-   
+    height:100%;
     display: grid;
     grid-template-columns: auto auto auto;
     grid-template-rows: auto auto;
