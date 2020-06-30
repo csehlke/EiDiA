ffd3d15b81b0458fbc5a8d3ea8f7b2aab70897fa
Philipp Bock <philipp.bock@hotmail.de>
2020-06-30 18:00:25 +0200

    changed layout of dashboard to grid layout and fittet Widget to it

--- EiDiA_frontend/src/components/fileCabinet/Dashboard.js
+++ EiDiA_frontend/src/components/fileCabinet/Dashboard.js
@@ -1,55 +1,61 @@
 import React from 'react';
 import styled from "styled-components";
 import {Widget} from "./Widgets/Widget";
-import {LogWidget} from "./Widgets/Log";
-import {logs,WidgetTypes} from "../Constants";
+import {Widgets} from "../Constants";
 
-const FlexRow = styled.div`
+
+const Grid = styled.div`
     height: 50vh;
     margin: 0 5% 0 5%;
-    display: flex;
-    flex-direction: row;
-    flex-wrap: wrap
-    padding: 50%;
+    
     text-align:center;
     vertical-align:middle;
+    display: grid;
+    grid-template-columns: auto auto auto;
+    grid-template-rows: auto auto;
     
     
 
 `;
 
 
-
 export class Dashboard extends React.Component {
     constructor(props) {
         super(props);
-        this.state={
+        this.state = {
+            widgets: Widgets
 
         }
     }
+
     render() {
         return (
-            <div>
-                <FlexRow>
-                    <Widget></Widget>
-                    <Widget></Widget>
-                    <Widget></Widget>
+            <Grid>
 
-                </FlexRow>
-                <FlexRow>
-                    <LogWidget >
-                        <p>Dashboard</p>
-                    </LogWidget>
-                    <LogWidget>
-                        <p>Dashboard</p>
-                    </LogWidget>
-                    <LogWidget>
-                        <p>Dashboard</p>
-                    </LogWidget>
-                </FlexRow>
+                {this.state.widgets.map((widget) => <Widget positionInfo={widget.positionInfo}/>)}
+
+            </Grid>
+            /* <div>
+                 <FlexRow>
+                     <Widget></Widget>
+                     <Widget></Widget>
+                     <Widget></Widget>
+
+                 </FlexRow>
+                 <FlexRow>
+                     <LogWidget >
+                         <p>Dashboard</p>
+                     </LogWidget>
+                     <LogWidget>
+                         <p>Dashboard</p>
+                     </LogWidget>
+                     <LogWidget>
+                         <p>Dashboard</p>
+                     </LogWidget>
+                 </FlexRow>
 
 
-            </div>
+             </div>*/
         );
     }
 }
--- EiDiA_frontend/src/components/fileCabinet/Widgets/Widget.js
+++ EiDiA_frontend/src/components/fileCabinet/Widgets/Widget.js
@@ -1,13 +1,17 @@
 import React from 'react';
-import {AiFillDelete, AiFillEdit} from 'react-icons/ai'
-import {FaCloudDownloadAlt} from 'react-icons/fa'
 import styled from "styled-components";
-import {IconContext} from "react-icons";
 
 const Body = styled.div`
-     width:30%;
+     /*width:30%;
      height: auto;
-     font-size:3vw;
+     font-size:3vw;*/
+     text-align:center;
+     
+     border: 2px dotted ${props => props.color};
+     grid-row-start:${props => props.positionInfo.y};
+     grid-row-end:${props => props.positionInfo.rows};
+     grid-column-start:${props => props.positionInfo.x};
+     grid-column-end:${props => props.positionInfo.cols};
      
 
 `;
@@ -15,25 +19,35 @@ const Body = styled.div`
  * TODO:
  *
  */
-const IconSize = '1.5vw'
 
 export class Widget extends React.Component {
     constructor(props) {
         super(props);
+        this.state = {
+            positionInfo: this.props.positionInfo,
+
+            color: "red"
+
+
+        }
+
     }
 
 
 
-    MainPart(){
-    let result = (
-        <Body>
-            {this.childPart()}
-        </Body>
+    MainPart() {
+        return (
+            <Body positionInfo={this.state.positionInfo} color={this.state.color}>
+                {/*this.childPart()+*/<div>
+                    <p>x/y</p>
+                    <p>{this.state.positionInfo.x + "," + this.state.positionInfo.y}</p>
+                    <p>Rows/Cols</p>
+                    <p>{this.state.positionInfo.rows + "," + this.state.positionInfo.cols}</p>
+                </div>}
+            </Body>
 
 
-
-    )
-        return result;
+        )
 
     }
     childPart(){
