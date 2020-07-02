8e1df796c79c6464070208a9424b392426baa9cc
Carsten Sehlke <carsten.sehlke@tum.de>
2020-07-02 21:21:23 +0200

    create advanced search query

--- EiDiA_frontend/src/assets/CompareTypes.js
+++ EiDiA_frontend/src/assets/CompareTypes.js
@@ -6,11 +6,11 @@ let CompareTypes = [
     {type: 'text', name: 'is equal to', id: 101, fields: 1},
     {type: 'text', name: 'begins with', id: 102, fields: 1},
     {type: 'text', name: 'ends with', id: 103, fields: 1},
+    {type: 'text', name: 'contains', id: 104, fields: 1},
 
-    {type: 'date', name: 'is equal to', id: 201, fields: 1},
-    {type: 'date', name: 'before', id: 202, fields: 1},
-    {type: 'date', name: 'after', id: 203, fields: 1},
-    {type: 'date', name: 'between', id: 204, fields: 2},
+    {type: 'date', name: 'before', id: 201, fields: 1},
+    {type: 'date', name: 'after', id: 202, fields: 1},
+    {type: 'date', name: 'between', id: 203, fields: 2},
 
     {type: 'number', name: 'is equal to', id: 301, fields: 1},
     {type: 'number', name: 'greater than', id: 302, fields: 1},
