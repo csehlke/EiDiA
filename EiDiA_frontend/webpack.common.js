73b566fb3e53abc550bc0785512f01ac3ba74d29
Philipp Bock <philipp.bock@hotmail.de>
2020-07-11 13:18:20 +0200

    Updated babel to version 7 and added class properties platform (allows e.g. arrow function in class property)

--- EiDiA_frontend/webpack.common.js
+++ EiDiA_frontend/webpack.common.js
@@ -25,7 +25,7 @@ module.exports = {
                 use: {
                     loader: 'babel-loader',
                     options: {
-                        presets: ['env', 'react']
+                        presets: ['@babel/preset-env', '@babel/preset-react']
                     }
                 }
             },
