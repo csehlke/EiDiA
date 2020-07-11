7fac5967598c1112e0b9220377f6c9c69207bea0
Carsten Sehlke <carsten.sehlke@tum.de>
2020-07-11 16:50:02 +0200

    connect auth admin backend to frontend

--- EiDiA_frontend/src/components/login/UserTable.js
+++ EiDiA_frontend/src/components/login/UserTable.js
@@ -17,6 +17,44 @@ import {
     RiDeleteBinLine,
     RiSave3Line
 } from "react-icons/all";
+import UserService from "../../services/UserService";
+
+
+const tableIcons = {
+    Add: React.forwardRef((props, ref) => <span ref={ref}> <IoMdAddCircle {...props} /> </span>),
+    Check: React.forwardRef((props, ref) => <span ref={ref}> <MdCheck {...props} /> </span>),
+    Clear: React.forwardRef((props, ref) => <span ref={ref}> <MdClear {...props} /> </span>),
+    Delete: React.forwardRef((props, ref) => <span ref={ref}> <RiDeleteBinLine {...props} /> </span>),
+    DetailPanel: React.forwardRef((props, ref) => <span ref={ref}> <MdChevronRight {...props} /> </span>),
+    Edit: React.forwardRef((props, ref) => <span ref={ref}> <MdEdit {...props} /> </span>),
+    Export: React.forwardRef((props, ref) => <span ref={ref}> <RiSave3Line {...props} /> </span>),
+    Filter: React.forwardRef((props, ref) => <span ref={ref}> <MdFilterList {...props} /> </span>),
+    FirstPage: React.forwardRef((props, ref) => <span ref={ref}> <MdFirstPage {...props} /> </span>),
+    LastPage: React.forwardRef((props, ref) => <span ref={ref}> <MdLastPage {...props} /> </span>),
+    NextPage: React.forwardRef((props, ref) => <span ref={ref}> <MdChevronRight {...props} /> </span>),
+    PreviousPage: React.forwardRef((props, ref) => <span ref={ref}> <MdChevronLeft {...props} /> </span>),
+    ResetSearch: React.forwardRef((props, ref) => <span ref={ref}> <MdClear {...props} /> </span>),
+    Search: React.forwardRef((props, ref) => <span ref={ref}> <AiOutlineSearch {...props} /> </span>),
+    SortArrow: React.forwardRef((props, ref) => <span ref={ref}> <MdArrowDownward {...props} /> </span>),
+    ThirdStateCheck: React.forwardRef((props, ref) => <span ref={ref}> <MdRemove {...props} /> </span>),
+    ViewColumn: React.forwardRef((props, ref) => <span ref={ref}> <MdViewColumn {...props} /> </span>)
+};
+
+const columns = [
+    {title: 'ID', field: 'id', readonly: true, hidden: true},
+    {title: 'Username', field: 'username', readonly: true, defaultSort: 'asc'},
+    {title: 'First Name', field: 'firstName'},
+    {title: 'Last Name', field: 'lastName'},
+    {title: 'Work Location', field: 'workLocation'},
+    {title: 'Position', field: 'workPosition'},
+    {title: 'Password', field: 'password', sorting: false, searchable: false},
+    {
+        title: 'User Role',
+        field: 'userRole',
+        lookup: {admin: 'Admin', user: 'Default'},
+        searchable: false,
+    },
+];
 
 export default class UserTable extends React.Component {
 
@@ -24,116 +62,87 @@ export default class UserTable extends React.Component {
         super(props);
 
         this.state = {
-            columns: [
-                {title: 'Username', field: 'username', readonly: true, defaultSort: 'asc'},
-                {title: 'First Name', field: 'firstName'},
-                {title: 'Last Name', field: 'lastName'},
-                {title: 'Work Location', field: 'location'},
-                {title: 'Position', field: 'position'},
-                {title: 'Password', field: 'password', sorting: false, searchable: false},
-                {
-                    title: 'User Role',
-                    field: 'role',
-                    lookup: {admin: 'Admin', user: 'Default'},
-                    searchable: false,
-                },
-            ],
-            data: [
-                {
-                    username: 'carsten',
-                    firstName: 'Carsten',
-                    lastName: 'Sehlke',
-                    location: 'Munich',
-                    position: 'Software Engineer',
-                    password: '⬤⬤⬤⬤⬤',
-                    role: 'admin'
-                },
-                {
-                    username: 'carsten1',
-                    firstName: 'Carsten',
-                    lastName: 'Sehlke',
-                    location: 'Munich',
-                    position: 'Software Engineer',
-                    password: '⬤⬤⬤⬤⬤',
-                    role: 'admin'
-                },
-                {
-                    username: 'carsten2',
-                    firstName: 'Carsten',
-                    lastName: 'Sehlke',
-                    location: 'Munich',
-                    position: 'Software Engineer',
-                    password: '⬤⬤⬤⬤⬤',
-                    role: 'admin'
-                },
-            ],
+            data: [],
         }
     }
 
-    render() {
-        const tableIcons = {
-            Add: React.forwardRef((props, ref) => <span ref={ref}> <IoMdAddCircle {...props} /> </span>),
-            Check: React.forwardRef((props, ref) => <span ref={ref}> <MdCheck {...props} /> </span>),
-            Clear: React.forwardRef((props, ref) => <span ref={ref}> <MdClear {...props} /> </span>),
-            Delete: React.forwardRef((props, ref) => <span ref={ref}> <RiDeleteBinLine {...props} /> </span>),
-            DetailPanel: React.forwardRef((props, ref) => <span ref={ref}> <MdChevronRight {...props} /> </span>),
-            Edit: React.forwardRef((props, ref) => <span ref={ref}> <MdEdit {...props} /> </span>),
-            Export: React.forwardRef((props, ref) => <span ref={ref}> <RiSave3Line {...props} /> </span>),
-            Filter: React.forwardRef((props, ref) => <span ref={ref}> <MdFilterList {...props} /> </span>),
-            FirstPage: React.forwardRef((props, ref) => <span ref={ref}> <MdFirstPage {...props} /> </span>),
-            LastPage: React.forwardRef((props, ref) => <span ref={ref}> <MdLastPage {...props} /> </span>),
-            NextPage: React.forwardRef((props, ref) => <span ref={ref}> <MdChevronRight {...props} /> </span>),
-            PreviousPage: React.forwardRef((props, ref) => <span ref={ref}> <MdChevronLeft {...props} /> </span>),
-            ResetSearch: React.forwardRef((props, ref) => <span ref={ref}> <MdClear {...props} /> </span>),
-            Search: React.forwardRef((props, ref) => <span ref={ref}> <AiOutlineSearch {...props} /> </span>),
-            SortArrow: React.forwardRef((props, ref) => <span ref={ref}> <MdArrowDownward {...props} /> </span>),
-            ThirdStateCheck: React.forwardRef((props, ref) => <span ref={ref}> <MdRemove {...props} /> </span>),
-            ViewColumn: React.forwardRef((props, ref) => <span ref={ref}> <MdViewColumn {...props} /> </span>)
-        };
+    componentDidMount() {
+        UserService.getAllUsersForAdministration().then((data) => {
+            this.setState({
+                data: [...data.users],
+            });
+        }).catch((e) => {
+            console.error(e);
+        });
+    }
 
+    render() {
         return (
             <MaterialTable title="Users"
-                           columns={this.state.columns}
+                           columns={columns}
                            data={this.state.data}
                            icons={tableIcons}
                            editable={{
-                               isDeletable: rowData => rowData.role !== "admin",
+                               isDeletable: rowData => rowData.userRole !== "admin",
                                onRowAdd: (newData) =>
-                                   new Promise((resolve) => {
-                                       setTimeout(() => {
-                                           resolve();
-                                           this.setState((prevState) => {
-                                               const data = [...prevState.data];
-                                               newData['password'] = '⬤⬤⬤⬤⬤';
-                                               data.push(newData);
-                                               return {...prevState, data};
-                                           });
-                                       }, 600);
-                                   }),
-                               onRowUpdate: (newData, oldData) =>
-                                   new Promise((resolve) => {
-                                       setTimeout(() => {
-                                           resolve();
-                                           if (oldData) {
+                                   new Promise((resolve, reject) => {
+                                       const user = {
+                                           username: newData.username,
+                                           firstName: newData.firstName,
+                                           lastName: newData.lastName,
+                                           password: newData.password,
+                                           workPosition: newData.workPosition,
+                                           workLocation: newData.workLocation,
+                                           userRole: newData.userRole,
+                                       }
+                                       UserService.addUserAdmin(user)
+                                           .then(newUser => {
+                                               resolve();
                                                this.setState((prevState) => {
                                                    const data = [...prevState.data];
-                                                   newData['password'] = '⬤⬤⬤⬤⬤';
-                                                   data[data.indexOf(oldData)] = newData;
+                                                   data.push(newUser);
                                                    return {...prevState, data};
                                                });
-                                           }
-                                       }, 600);
+                                           })
+                                           .catch(() => reject());
+                                   }),
+                               onRowUpdate: (newData, oldData) =>
+                                   new Promise((resolve, reject) => {
+                                       const user = {
+                                           id: oldData.id,
+                                           username: oldData.username,
+                                           firstName: newData.firstName,
+                                           lastName: newData.lastName,
+                                           password: newData.password === '⬤⬤⬤⬤⬤' ? null : newData.password,
+                                           workPosition: newData.workPosition,
+                                           workLocation: newData.workLocation,
+                                           userRole: newData.userRole,
+                                       }
+                                       UserService.updateUserAdmin(user)
+                                           .then(newUser => {
+                                               if (oldData) {
+                                                   this.setState((prevState) => {
+                                                       const data = [...prevState.data];
+                                                       data[data.indexOf(oldData)] = newUser;
+                                                       return {...prevState, data};
+                                                   });
+                                               }
+                                               resolve();
+                                           })
+                                           .catch(() => reject());
                                    }),
                                onRowDelete: (oldData) =>
-                                   new Promise((resolve) => {
-                                       setTimeout(() => {
-                                           resolve();
-                                           this.setState((prevState) => {
-                                               const data = [...prevState.data];
-                                               data.splice(data.indexOf(oldData), 1);
-                                               return {...prevState, data};
-                                           });
-                                       }, 600);
+                                   new Promise((resolve, reject) => {
+                                       UserService.deleteUserAdmin(oldData.id)
+                                           .then(() => {
+                                               this.setState((prevState) => {
+                                                   const data = [...prevState.data];
+                                                   data.splice(data.indexOf(oldData), 1);
+                                                   return {...prevState, data};
+                                               });
+                                               resolve();
+                                           })
+                                           .catch(() => reject());
                                    }),
                            }}
             />
