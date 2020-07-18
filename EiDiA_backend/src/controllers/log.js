374abaccb1b3dc4fea316d06320312ecf59d7a33
Philipp Bock <philipp.bock@hotmail.de>
2020-07-18 16:00:27 +0200

    added recentRecords to Log controller

--- EiDiA_backend/src/controllers/log.js
+++ EiDiA_backend/src/controllers/log.js
@@ -32,8 +32,39 @@ const getLogs = (req, res) => {
         });
 
 };
+const getRecentRecords = (req, res) => {
+    LogModel.aggregate([{$match: {userId: mongoose.Types.ObjectId(req.params.userId)}},
+            {$sort: {date: -1}},
+            {$group: {_id: '$recordId',}},
+            {$lookup: {from: 'records', localField: '_id', foreignField: '_id', as: 'records'}},
+            {$unwind: "$records"},
+            {
+                "$project": {
+                    "_id": 0,
+                    "recordId": "$records._id",
+                    "name": "$records.name"
+
+                }
+            }
+        ],
+        function (err, logs) {
+            console.log(logs)
+            if (err)
+                res.status(400).json({
+                    error: 'Internal server error',
+                    message: err.message,
+
+                });
+            else {
+                res.status(200).json(logs.slice(0, 5));
+
+            }
+
+        });
+}
 
 
 module.exports = {
     getLogs,
+    getRecentRecords,
 };
--- EiDiA_backend/src/routes/log.js
+++ EiDiA_backend/src/routes/log.js
@@ -9,7 +9,7 @@ const LogController = require('../controllers/log');
 //TODO:activate authentication for development
 
 router.get('/list/:recordId', LogController.getLogs);
-
+router.get('/recentRecords/list/:userId', LogController.getRecentRecords)
 /*router.get('/list', middleWares.checkAuthentication, RecordController.listRecords);
 router.post('/add', middleWares.checkAuthentication, RecordController.addRecord);*/
 module.exports = router;
