97048ec7ad77c5f09020cdaf93193e6e5692c9d8
Tom Bader <tom@tombader.de>
2020-07-18 16:25:56 +0200

    Removed unused code

--- EiDiA_backend/src/controllers/log.js
+++ EiDiA_backend/src/controllers/log.js
@@ -2,36 +2,6 @@
 const LogModel = require('../models/log');
 const mongoose = require('mongoose');
 
-
-const getLogs = (req, res) => {
-    LogModel.aggregate([{$match: {recordId: mongoose.Types.ObjectId(req.params.recordId)}},
-            {$sort: {date: -1}},
-            {$lookup: {from: 'users', localField: 'userId', foreignField: '_id', as: 'user'}},
-            {$unwind: "$user"},
-            {
-                "$project": {
-                    "_id": 0,
-                    "logID": "$_id",
-                    "action": "$log",
-                    "user": "$user.username",
-                    "date": "$date",
-
-                }
-            }
-        ],
-        function (err, logs) {
-            if (err)
-                res.status(400).json({
-                    error: 'Internal server error',
-                    message: error.message,
-
-                });
-            else
-                res.status(200).json(logs.slice(0, 20));
-
-        });
-
-};
 const getRecentRecords = (req, res) => {
     LogModel.aggregate([{$match: {userId: mongoose.Types.ObjectId(req.params.userId)}},
             {$sort: {date: -1}},
--- EiDiA_backend/src/routes/log.js
+++ EiDiA_backend/src/routes/log.js
@@ -6,9 +6,7 @@ const router = express.Router();
 const middleWares = require('../middlewares');
 const LogController = require('../controllers/log');
 
-//TODO:activate authentication for development
 
-router.get('/list/:recordId', LogController.getLogs);
 router.get('/recentRecords/list/:userId', middleWares.checkAuthentication, LogController.getRecentRecords)
 
 
