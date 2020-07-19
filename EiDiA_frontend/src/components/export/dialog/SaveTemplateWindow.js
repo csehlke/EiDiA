74b72770620a65e1d80355abee89bfdf89fe0bbf
neihtfool <q.thien.nguyen@outlook.de>
2020-07-19 17:10:10 +0200

    change styling for ExportDocument

--- EiDiA_frontend/src/components/export/dialog/SaveTemplateWindow.js
+++ EiDiA_frontend/src/components/export/dialog/SaveTemplateWindow.js
@@ -7,17 +7,6 @@ import withStyles from "@material-ui/core/styles/withStyles";
 const styles = theme => ({
     error: {
         color: theme.palette.error.main // see index.js
-    },
-    button_left: {
-        margin: "15px",
-        align: "left",
-        bottom: 0,
-    },
-    button_right: {
-        margin: "15px",
-        align: "right",
-        bottom: 0,
-        right: 0,
     }
 });
 
@@ -36,24 +25,27 @@ class SaveTemplateWindow extends React.Component {
                 </Row>
                 Saving template not implemented yet
                 <Row>
-                    <Button
-                        style={styles.button_left}
-                        variant="contained"
-                        disableElevation
-                        onClick={this.props.onClose}
-                        className={classes.error}
-                    >
-                        Cancel
-                    </Button>
-                    <Button
-                        style={styles.button_right}
-                        variant="contained"
-                        color="primary"
-                        disableElevation
-                        onClick={this.props.save}
-                    >
-                        Save Template
-                    </Button>
+                    <div style={{margin: "15px"}}>
+                        <Button
+                            variant="contained"
+                            disableElevation
+                            onClick={this.props.onClose}
+                            className={classes.error}
+                        >
+                            Cancel
+                        </Button>
+                    </div>
+                    <div style={{margin: "15px"}}>
+                        <Button
+                            style={styles.button_right}
+                            variant="contained"
+                            color="primary"
+                            disableElevation
+                            onClick={this.props.save}
+                        >
+                            Save Template
+                        </Button>
+                    </div>
                 </Row>
             </Column>
         )
