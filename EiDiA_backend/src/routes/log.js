29e9e3d566150685430b52780ebce5973f4a7b51
Philipp Bock <philipp.bock@hotmail.de>
2020-07-19 15:17:09 +0200

    refactored log controller

--- EiDiA_backend/src/routes/log.js
+++ EiDiA_backend/src/routes/log.js
@@ -6,10 +6,8 @@ const router = express.Router();
 const middleWares = require('../middlewares');
 const LogController = require('../controllers/log');
 
-//TODO:activate authentication for development
 
-router.get('/list/:recordId', LogController.getLogs);
-router.get('/recentRecords/list/:userId', LogController.getRecentRecords)
-/*router.get('/list', middleWares.checkAuthentication, RecordController.listRecords);
-router.post('/add', middleWares.checkAuthentication, RecordController.addRecord);*/
+router.get('/list/:recordId', middleWares.checkAuthentication, LogController.getLogs);
+router.get('/recentRecords/list/:userId', middleWares.checkAuthentication, LogController.getRecentRecords)
+
 module.exports = router;
